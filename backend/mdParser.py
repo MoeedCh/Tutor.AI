26f51e0b50b4217ba9cfeed7ac9319573105363a
MoeedCh <moeedchaudhry15@gmail.com>
2023-09-16 18:29:26 -0400

    finished md parser

--- backend/mdParser.py
+++ backend/mdParser.py
@@ -1,12 +1,15 @@
 import mistletoe
 from bs4 import BeautifulSoup
 
-with open('test.md','r') as fin:
-    rendered = mistletoe.markdown(fin)
-    
-    soup = BeautifulSoup(rendered, 'html.parser')
+def fetchKeyConcepts(filename):
+    with open(filename,'r') as fin:
+        rendered = mistletoe.markdown(fin)
+        
+        soup = BeautifulSoup(rendered, 'html.parser')
 
-    h1_tags = soup.find_all('h1')
+        h1_tags = soup.find_all('h1')
 
-    print(h1_tags)
-
+        result = []
+        for h1 in h1_tags:
+            result.append(h1.text.split(": ")[1])
+        return result
