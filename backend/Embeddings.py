99ab15d8b1d7f0f301f9abd10cc82f79ce5ad6d2
andrew <andrew.neeser@me.com>
2023-09-16 14:20:24 -0400

    embedding library

--- backend/Embeddings.py
+++ backend/Embeddings.py
@@ -22,14 +22,14 @@ class VectorDatabase:
     def __init__(self, embeddings=GPT4AllEmbeddings()):
         self.embeddings = embeddings
 
-    def create_index(self, index_name, split_text):
+    def create_index(self, index_name, folder, split_text):
         for i in split_text:
             db = None
             if isinstance(split_text[0], str):
                 db = FAISS.from_texts(split_text, self.embeddings)
             else:
                 db = FAISS.from_documents(split_text, self.embeddings)
-            db.save_local("faiss_db", index_name=index_name)
+            db.save_local("faiss_db/" + folder, index_name=index_name)
 
     def load_index(self, index_name):
         db = FAISS.load_local(
@@ -74,20 +74,20 @@ class VectorDatabase:
     def add_chapters_from_epub(self, epub_path):
         split_epub = self.add_epub(epub_path)
         for i in range(len(split_epub)):
-            self.create_index("Chapter " + str(i), split_epub[i])
+            self.create_index("Chapter " + str(i), os.path.basename(epub_path),split_epub[i])
 
 
 vdb = VectorDatabase()
-#vdb.add_chapters_from_epub("../bookdata/Dune{Frank_Herbert}.epub")
+vdb.add_chapters_from_epub("../bookdata/2018FundamentalsOfCppProgramming_2018_fundamentals-of-cpp-programming.epub")
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo")
-
-qa = ConversationalRetrievalChain.from_llm(llm, retriever=vdb.load_index("Chapter 6").as_retriever())
-question = "Write me a detailed sumamry of the provided text"
-chat_history = []
-
-result = qa({"question": question, "chat_history": chat_history})
-chat_history.append((question, result["answer"]))
-print(f"Question: \n {question} \n")
-print(f"Answer: \n {result['answer']} \n")
-print("------------------------------------------------------- \n")
+# llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+#
+# qa = ConversationalRetrievalChain.from_llm(llm, retriever=vdb.load_index("python-books_A_Practical_Introduction_to_Python_Programming_Heinold.epub/Chapter 22").as_retriever())
+# question = "Restructure this content into its Key Concepts. Under each Key Concept, provide a detailed explanation. Serve the response in Markdown format."
+# chat_history = []
+#
+# result = qa({"question": question, "chat_history": chat_history})
+# chat_history.append((question, result["answer"]))
+# print(f"Question: \n {question} \n")
+# print(f"Answer: \n {result['answer']} \n")
+# print("------------------------------------------------------- \n")
