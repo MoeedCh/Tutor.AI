057abb64e0baf0f2d20ae1a0497fddf2db14be17
andrew <andrew.neeser@me.com>
2023-09-16 18:03:15 -0400

    Generate Markdown

--- backend/Embeddings.py
+++ backend/Embeddings.py
@@ -7,6 +7,8 @@ from langchain.document_loaders import TextLoader, PyPDFLoader, UnstructuredEPub
 from langchain.embeddings import GPT4AllEmbeddings
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 import hashlib
 import ebooklib
 from ebooklib import epub
@@ -37,12 +39,23 @@ class VectorDatabase:
         )
         return db
 
-    def split_text(self, loader, chunk_size=1000, chunk_overlap=0):
+    # def split_text(self, loader, chunk_size=1000, chunk_overlap=0):
+    #     documents = []
+    #     for l in loader:
+    #         text_splitter = CharacterTextSplitter(
+    #             chunk_size=chunk_size, chunk_overlap=chunk_overlap
+    #         )
+    #         if isinstance(l, str):
+    #             documents.append(text_splitter.split_text(l))
+    #         else:
+    #             documents.append(text_splitter.split_documents(l))
+    #     return documents
+
+
+    def split_text(self, loader, max_chunk_size=2000):
         documents = []
         for l in loader:
-            text_splitter = CharacterTextSplitter(
-                chunk_size=chunk_size, chunk_overlap=chunk_overlap
-            )
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=max_chunk_size, chunk_overlap=0)
             if isinstance(l, str):
                 documents.append(text_splitter.split_text(l))
             else:
@@ -63,23 +76,28 @@ class VectorDatabase:
         loader = text
         return self.split_text(loader)
 
+
     def add_chapters_from_epub(self, epub_path):
         split_epub = self.add_epub(epub_path)
+        i = 0
         for i in range(len(split_epub)):
+            print("Creating index for chapter " + str(i))
             self.create_index("Chapter " + str(i), os.path.basename(epub_path),split_epub[i])
+        return i
 
-
-vdb = VectorDatabase()
-# vdb.add_chapters_from_epub("../bookdata/2018FundamentalsOfCppProgramming_2018_fundamentals-of-cpp-programming.epub")
-
-llm = ChatOpenAI(model_name="gpt-3.5-turbo")
-
-qa = ConversationalRetrievalChain.from_llm(llm, retriever=vdb.load_index("2018FundamentalsOfCppProgramming_2018_fundamentals-of-cpp-programming.epub/Chapter 22").as_retriever())
-question = "Restructure this content into its Key Concepts. Under each Key Concept, provide a detailed explanation. Serve the response in Markdown format."
-chat_history = []
-
-result = qa({"question": question, "chat_history": chat_history})
-chat_history.append((question, result["answer"]))
-print(f"Question: \n {question} \n")
-print(f"Answer: \n {result['answer']} \n")
-print("------------------------------------------------------- \n")
+# vdb = VectorDatabase()
+# print(vdb.add_chapters_from_epub("../bookdata/Dune{Frank_Herbert}.epub"))
+#
+# llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+#
+# qa = ConversationalRetrievalChain.from_llm(llm, retriever=vdb.load_index("2018FundamentalsOfCppProgramming_2018_fundamentals-of-cpp-programming.epub/Chapter 22").as_retriever())
+# #question = "Restructure this content into its Key Concepts. Under each Key Concept, provide a detailed explanation. Serve the response in Markdown format."
+# chat_history = []
+# import json
+# json = json.load(open("Prompts.json"))
+# question = json["Prompts"]["ExamplePrompt"]
+# result = qa({"question": question, "chat_history": chat_history})
+# chat_history.append((question, result["answer"]))
+# print(f"Question: \n {question} \n")
+# print(f"Answer: \n {result['answer']} \n")
+# print("------------------------------------------------------- \n")
---
+++ backend/Server.py
@@ -0,0 +1,52 @@
+import os
+
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from backend.Embeddings import VectorDatabase
+from multiprocessing import Pool
+
+
+class Runner:
+
+    def __init__(self):
+        self.llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+        self.vdb = VectorDatabase()
+
+    def generate_markdown(self, path_to_epub, prompt):
+        # Vectorize the chapters in the book and returns the number of chapters
+        num_chapters = self.vdb.add_chapters_from_epub(path_to_epub)
+
+        os.makedirs("cache/" + os.path.basename(path_to_epub), exist_ok=True)
+
+        for i in range(num_chapters):
+            chat_history = []
+            qa = ConversationalRetrievalChain.from_llm(self.llm, retriever=self.vdb.load_index(
+                os.path.basename(path_to_epub) + "/Chapter " + str(i)).as_retriever())
+            result = qa({"question": prompt, "chat_history": chat_history})
+            with open("cache/" + os.path.basename(path_to_epub) + "/" + str(i) + ".md", "w+") as f:
+                f.write(str(result["answer"]))
+
+
+# llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+# vdb = VectorDatabase()
+# def worker(i, path_to_epub, prompt):
+#     chat_history = []
+#     print("pid: " + str(os.getpid()) + " i: " + str(i) + " path_to_epub: " + path_to_epub + " prompt: " + prompt)
+#     qa = ConversationalRetrievalChain.from_llm(llm, retriever=vdb.load_index(
+#         os.path.basename(path_to_epub) + "/Chapter " + str(i)).as_retriever())
+#     result = qa({"question": prompt, "chat_history": chat_history})
+#     with open("cache/" + os.path.basename(path_to_epub) + "/" + str(i) + ".md", "w+") as f:
+#         f.write(str(result["answer"]))
+#
+# def generate_markdown(path_to_epub, prompt):
+#     # Vectorize the chapters in the book and returns the number of chapters
+#     num_chapters = vdb.add_chapters_from_epub(path_to_epub)
+#
+#     os.makedirs("cache/" + os.path.basename(path_to_epub), exist_ok=True)
+#
+#     with Pool() as p:
+#         p.starmap(worker, [(i, path_to_epub, prompt) for i in range(num_chapters)])
+
+r = Runner()
+
+r.generate_markdown("../bookdata/python-books_A_Practical_Introduction_to_Python_Programming_Heinold.epub", "Restructure this content into its Key Concepts. Under each Key Concept, provide a detailed explanation. Serve the response in Markdown format use (###) to seperate the key concepts.")
