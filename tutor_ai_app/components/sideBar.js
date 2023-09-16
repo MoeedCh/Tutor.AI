b12ce39f1e0023a2a512ba6011857a089229b5c9
MoeedCh <moeedchaudhry15@gmail.com>
2023-09-16 02:32:40 -0400

    testHome has correct sizing now

--- tutor_ai_app/components/sideBar.js
+++ tutor_ai_app/components/sideBar.js
@@ -1,12 +1,15 @@
-// Component.js
-
 import React from 'react'
 
 const SideBar = () => {
 
   return (
-    <div>
-      <h1>My Component</h1>
+    <div className="flex flex-col h-screen overflow-y-auto">
+        <div className="p-4">
+            <h1 className="text-white">HELLO</h1>
+            <h1 className="text-white">HELLO FU</h1>
+            <h1 className="text-white">HELLO CK</h1>
+            <h1 className="text-white">HELLO ER</h1>
+        </div>
     </div>
   )
 
---
+++ tutor_ai_app/pages/testHome.js
@@ -0,0 +1,26 @@
+import NavBar from "@/components/navBar";
+import SideBar from "@/components/sideBar";
+
+export default function testHome() {
+
+    return (
+        <main>
+            <header class="bg-blue-500 text-white p-4"> 
+                <NavBar />
+            </header>
+        
+
+            <div className="grid grid-rows-4 grid-cols-6 min-h-screen">
+                {/* Sidebar */}
+                <aside className="row-span-4 col-span-1 bg-gray-600">
+                    <SideBar />
+                </aside>
+
+                {/* Main Content */}
+                <section className="row-span-4 col-span-5 bg-blue-300">
+                    {/* Main content here */}
+                </section>
+            </div>
+        </main>
+    );
+}
