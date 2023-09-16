551895bf33f63ff417148a37b04264e765ac2d74
MoeedCh <moeedchaudhry15@gmail.com>
2023-09-16 00:40:08 -0400

    moeed Branch

--- tutor_ai_app/pages/index.js
+++ tutor_ai_app/pages/index.js
@@ -1,7 +1,12 @@
 import Image from 'next/image'
+import '../styles/globals.css'
 
 export default function Home() {
   return (
-    <></>
+    <main className="flex h-screen">
+      <h1 className="m-auto text-3xl font-bold">
+        Welcome Home!
+      </h1>
+    </main>
   )
 }
